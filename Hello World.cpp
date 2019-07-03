ae7bf56423f959632017474bd87067a160bca2df
Megha Bisht <bishtmegha98@gmail.com>
2019-07-03 17:22:19 +0530

    First Example of C++

---
+++ Hello World.cpp
@@ -0,0 +1,8 @@
+#include <iostream>                     //#include is a preprocessor directive that causes compiler to replace that line with the text 
+using namespace std;                    //of contents of the named source file(if included in quotes "") or named header(if included in
+                                        //angle brackets<>)
+int main()                              //Execution of every C++ program begins with the main() function                      
+{
+cout<<"Hello World"<<endl;
+return 0;
+}
