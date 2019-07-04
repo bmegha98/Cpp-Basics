29b3d650ccef4e993deccc665ce2a160c8f3b551
Megha Bisht <bishtmegha98@gmail.com>
2019-07-04 17:22:04 +0530

    <bits/stdc++.h> header file

---
+++ special header file in C++.cpp
@@ -0,0 +1,17 @@
+/*
+ <bits/stdc++.h> : It is basically a header file that includes every standard library.
+ 
+ Using it would include a lot of unnecessary stuff and increases compilation time.
+ So, the user can either use it and save the time of writing every include or save the compilation time by not using it and 
+ writing necessary header files.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    cout<<x<<endl
+    return 0;
+}
