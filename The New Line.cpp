7b5013d399ba90f0e8040628123ebe73f653dbad
Megha Bisht <bishtmegha98@gmail.com>
2019-07-07 06:51:38 +0530

    Create The New Line.cpp

---
+++ The New Line.cpp
@@ -0,0 +1,22 @@
+/*
+You are familiar with producing output using CPP. In this task, you'll be required to write three messages in three separate lines. Write 
+Geeks for Geeks. But wait a minute!! You need to write each word of it in a separate line.
+In the function printInNewLine(), output each word of Geeks for Geeks in a separate line.
+*/
+
+#include<iostream>
+using namespace std;
+void printNewLine();
+//Position this line where user code will be pasted.
+int main(){
+    
+    printNewLine();
+    
+}
+void printNewLine(){
+    
+    cout<<"Geeks"<<endl;
+    cout<<"for"<<endl;
+    cout<<"Geeks"<<endl;
+    
+}
