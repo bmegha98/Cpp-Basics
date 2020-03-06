52453a230066736b8731ac80a3ee3426bb4c3bf9
Megha Bisht <bishtmegha98@gmail.com>
2020-03-06 13:51:37 +0530

    pop operation is costly

---
+++ Stacks/Queue using stacks.cpp
@@ -0,0 +1,28 @@
+//Implement a Queue using 2 stacks s1 and s2 .
+
+void StackQueue :: push(int x)
+ {
+        // Your Code
+        s1.push(x);
+ }
+
+/*The method pop which return the element poped out of the queue*/
+int StackQueue :: pop()
+{
+        // Your Code 
+        if(s1.empty() && s2.empty())
+            return -1;
+        if(s2.empty())
+        {
+            while(!s1.empty())
+            {
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        
+        int x = s2.top();
+        s2.pop();
+        
+        return x;
+}
